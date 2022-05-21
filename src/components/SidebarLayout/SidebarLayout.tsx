c6f8d69ac1b1c5e7e976eae0ef78a81290dcd856
ulazy1 <ulazy1@github.com>
2022-05-21 15:02:46 -0400

    simple layout with animated sidebar
    similar to the youtube sidebar

--- src/components/SidebarLayout/SidebarLayout.tsx
+++ src/components/SidebarLayout/SidebarLayout.tsx
@@ -1,12 +1,96 @@
-import React, { FC } from 'react';
+import React from 'react';
 import './SidebarLayout.css';
 
-interface SidebarLayoutProps {}
-
-const SidebarLayout: FC<SidebarLayoutProps> = () => (
-  <div className="SidebarLayout" data-testid="SidebarLayout">
-    SidebarLayout Component
-  </div>
+const text =
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium unde iusto quia atque quasi corrupti. Nulla tempora obcaecati dolor numquam dignissimos repellendus facilis doloremque accusantium quis. Possimus fuga blanditiis nisi nobis totam ipsum obcaecati vel, accusantium magnam explicabo ducimus aspernatur, quidem, labore quod temporibus? Officiis dolor quae cum obcaecati minus, blanditiis iste nisi veniam aperiam assumenda. Soluta incidunt natus numquam! Officiis facilis molestias beatae dolorum similique, tempora doloribus at dolor ullam, a quasi maxime provident doloremque molestiae officia. Tempora cumque molestias alias nesciunt asperiores, ipsum itaque officia iste expedita est dicta illo, laudantium provident! Consequuntur perferendis dolore, placeat voluptate, eaque non aspernatur voluptates vel dolorum molestiae fugiat quisquam provident dicta iste explicabo animi reiciendis sed aliquid necessitatibus quos corrupti? Ex, tenetur necessitatibus ipsum vero tempora et incidunt veniam est. Iure distinctio vitae molestias nihil sequi, aperiam explicabo labore ut dolorum accusamus? Quidem odit eveniet numquam officia tenetur exercitationem! Eaque voluptas deserunt saepe rem impedit in. Voluptatem aspernatur iusto commodi cupiditate fugit voluptate, pariatur magnam. Necessitatibus ipsum autem soluta delectus, culpa dolore ut sapiente quos dolor porro, architecto itaque modi cum voluptatum quod cumque eveniet doloremque neque, perspiciatis in distinctio officia ullam corporis? At accusamus tempora neque delectus beatae deleniti nam dolorum aspernatur vel sunt fuga reprehenderit tempore dolor corrupti, nemo eligendi adipisci reiciendis, necessitatibus consequuntur. Deserunt blanditiis rerum atque nisi consequuntur aspernatur eos voluptatibus, magni mollitia quia laudantium repudiandae tenetur quos architecto itaque! Expedita quam, incidunt nisi iure numquam veritatis aliquid dignissimos architecto quos amet vitae, maxime ex aspernatur impedit repudiandae voluptates officiis optio. Saepe officiis consequatur corporis, nulla sint, sed quas, hic est repellendus eos quisquam fugit vitae impedit mollitia facere tenetur! Temporibus error eaque consequatur corporis, vitae doloremque a, voluptatibus magnam tempore debitis facilis? Impedit sit architecto consequatur similique maxime quidem, tenetur laboriosam dolorum aspernatur unde beatae ex repudiandae, amet, provident rerum dicta molestias deleniti non. Aliquid sequi unde omnis, repellat eaque fuga qui maxime. Distinctio adipisci veritatis facilis dicta. Soluta quasi voluptatem temporibus, voluptatum optio illo deleniti mollitia error fugiat possimus, nesciunt odio dolore aperiam consequuntur totam voluptas ut nobis! Sed rem accusantium quae sapiente dolores! Velit odit ipsam odio quos officia obcaecati quod et beatae, sint repellendus blanditiis autem nesciunt, esse quae! Dolor perspiciatis necessitatibus architecto dignissimos fuga sapiente voluptas, voluptatibus amet rem at sit ut harum quae nihil quibusdam praesentium velit! Odio explicabo corrupti nostrum sapiente unde sed reiciendis eaque at neque quidem soluta ratione similique nihil vitae molestiae architecto dolorem magnam quisquam quod fugit incidunt alias nesciunt, accusantium rerum? Excepturi odit voluptate quaerat nam error, cumque, accusantium dolore nobis aspernatur veritatis quisquam? Modi autem corrupti eaque aliquam, laboriosam reiciendis excepturi nihil at porro ipsam deleniti exercitationem quaerat quam itaque eveniet quod nemo veritatis, tempore doloribus quo sunt minus rerum iste ut. Corporis ea, provident iste unde similique eius qui odit ratione consectetur quidem, atque nemo nesciunt illo, pariatur excepturi tempore minima dolores. Consequuntur quod, iste alias soluta quisquam sint odio repudiandae rem perspiciatis commodi dignissimos qui ratione ea distinctio esse quo debitis. Excepturi sint sequi minima dolorem harum repellat quaerat dignissimos veniam nihil ut placeat delectus dolor, ipsam commodi id enim totam cupiditate et eum in maxime. Veniam quisquam, ipsa optio modi ratione ea error molestiae, harum impedit ullam vitae eos nostrum quas facere numquam consequatur placeat dolore eum commodi alias magni? Est nemo aliquid sint. Corporis sunt libero laborum qui sit autem porro! Consequuntur neque dolores officia quisquam cupiditate. Unde veritatis reiciendis doloremque? Consequuntur corrupti fugiat voluptas porro vitae nobis qui, quaerat, ullam architecto veniam quae doloribus minima optio blanditiis velit a cum ea, inventore quo doloremque nostrum voluptate nesciunt. Excepturi vero aperiam facilis unde deserunt atque quia quis fugiat corrupti quaerat qui necessitatibus non, dolor incidunt ducimus cumque, aspernatur molestias nemo sed! Culpa vel molestiae repudiandae aliquam? Sed ipsum deserunt hic quis veritatis sit ratione, recusandae incidunt consequuntur est, harum eaque perferendis voluptatum repellendus voluptatibus id fugit totam. Similique veniam dolor laudantium, voluptates natus perspiciatis animi sit laboriosam nostrum, corporis itaque saepe sapiente, delectus dolorum nobis ullam sed soluta harum illum maxime! Earum nesciunt, obcaecati laboriosam placeat voluptates molestiae ipsa aspernatur odio illum aperiam error? Tempora maiores sequi nesciunt deserunt reprehenderit porro autem quos natus mollitia. Repellendus unde incidunt quaerat eveniet, expedita atque sapiente repellat in molestias odit omnis, voluptatum voluptas. Exercitationem veritatis voluptate dicta fuga, sit inventore recusandae officia minus corporis labore animi velit iusto suscipit ipsum temporibus, qui vero repellendus culpa tempora reprehenderit. Quasi non et eaque numquam labore eveniet magni dolor necessitatibus veniam ab voluptas architecto recusandae est, nesciunt mollitia, reiciendis laborum optio porro? Tenetur illo, suscipit accusamus maxime voluptas corporis? Repellat, nisi sunt amet eum libero facilis blanditiis suscipit nemo ab nihil assumenda quis beatae soluta iure? Totam soluta nulla quasi aliquid! Quo fugit a officia iure praesentium, nisi expedita sequi, quibusdam velit repellendus et quas esse in minima voluptatem modi dolorem aspernatur. Expedita omnis qui molestiae ea iure impedit praesentium consequuntur harum sed, at quae ad delectus deserunt itaque ducimus excepturi! Quod nesciunt deleniti eos saepe, sapiente earum explicabo, consectetur sed, ducimus dignissimos odio pariatur! Sit dolore ipsam itaque ad reprehenderit vitae tempora nulla esse pariatur, quae perspiciatis suscipit error, facilis accusamus assumenda doloremque nostrum ut doloribus cumque. Culpa ut eos dolore hic aliquid ipsum cum aperiam ab recusandae blanditiis sint eligendi, voluptates nam voluptatem harum omnis? Delectus voluptates sit voluptatibus hic laudantium dolore asperiores facilis cum pariatur, autem ducimus laborum, minima officiis. Voluptates, perferendis assumenda iure sint excepturi recusandae odit hic eius officia sunt dolores reprehenderit facere accusamus, quae numquam eveniet asperiores, suscipit animi? Voluptatum ipsum animi dicta, sint vitae aliquid, corporis soluta, impedit eligendi sed nisi amet accusamus iste voluptate porro tenetur sapiente necessitatibus! Modi vitae autem adipisci unde ea nesciunt libero. In dignissimos amet hic laudantium tenetur cum earum eaque consectetur cumque suscipit, aperiam magni perferendis sed dolor non? Pariatur saepe quam natus suscipit fugit. Aperiam fuga ipsum ipsam minus consequuntur commodi voluptatum cum repellendus quasi, minima totam vel omnis voluptas earum rerum amet fugit modi architecto mollitia ex, ut id assumenda magnam voluptates!';
+const menuSvg = (
+  <svg
+    viewBox='0 0 24 24'
+    preserveAspectRatio='xMidYMid meet'
+    focusable='false'
+  >
+    <g>
+      <path d='M21,6H3V5h18V6z M21,11H3v1h18V11z M21,17H3v1h18V17z'></path>
+    </g>
+  </svg>
 );
+
+interface Props {}
+
+interface State {
+  menuVisible: boolean;
+}
+
+class SidebarLayout extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      menuVisible: false,
+    };
+  }
+  onClick = () => {
+    const newMenuVisible = !this.state.menuVisible;
+    this.setState({
+      menuVisible: newMenuVisible,
+    });
+    if (newMenuVisible) {
+      window.addEventListener(
+        'keydown',
+        (event) => {
+          if (event.key === 'Escape') {
+            this.setState({
+              menuVisible: false,
+            });
+          }
+        },
+        { once: true }
+      );
+    }
+  };
+
+  render() {
+    console.log('rendering');
+    return (
+      <div className='SidebarLayout' data-testid='SidebarLayout'>
+        <header className='header'>
+          <div className='menu-icon' onClick={this.onClick}>
+            {menuSvg}
+          </div>
+        </header>
+
+        <div className='container'>
+          <div className='fixed-wrapper'>
+            <aside
+              className='sidebar'
+              style={{
+                animationName: this.state.menuVisible
+                  ? 'slide-in'
+                  : 'slide-out',
+              }}
+            >
+              <div className='menu-icon' onClick={this.onClick}>
+                {menuSvg}
+              </div>
+              {Array.from(Array(30).keys()).map((i) => {
+                return <div className='sidebar-item'>Item {i}</div>;
+              })}
+            </aside>
+            {/* {this.state.menuVisible && (
+            <aside className='sidebar'>
+              <div className='sidebar-item'>Testing 1234</div>
+              <div className='sidebar-item'>Another one</div>
+              <div className='sidebar-item'>And another</div>
+            </aside>
+          )} */}
+          </div>
+          <main className='content'>
+            <h1>Title</h1>
+            {text}
+          </main>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default SidebarLayout;

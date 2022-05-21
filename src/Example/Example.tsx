c934aee734eeaa0a7b36040f0e46802c6defcc27
ulazy1 <ulazy1@github.com>
2022-05-21 12:18:54 -0400

    remove unnecessary files

---
+++ src/Example/Example.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './Example.css';
+
+interface Props {}
+
+interface State {}
+
+class Example extends React.Component<Props, State> {
+  render() {
+    return <h1>Example</h1>;
+  }
+}
+
+export default Example;

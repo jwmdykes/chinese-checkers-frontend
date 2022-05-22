c12a1b2a14f5ef3285d5bf1a2b1c450f0d49600b
ulazy1 <ulazy1@github.com>
2022-05-21 21:20:46 -0400

    working board with clicking

--- src/components/Game/Board/Board.tsx
+++ src/components/Game/Board/Board.tsx
@@ -1,46 +1,88 @@
-import React, { createRef } from 'react';
+import React from 'react';
 import './Board.css';
+
+interface RowProps {
+  row: Array<Number>;
+  rowNumber: Number;
+  colors: Array<String>;
+  clickCallback(e: React.MouseEvent): void;
+}
+
+interface RowState {}
+
+class Row extends React.Component<RowProps, RowState> {
+  render() {
+    return (
+      <div className='Row'>
+        {this.props.row.map((value: Number, index: Number) => {
+          const color = '' + this.props.colors[+value];
+          return (
+            <span
+              className='BallHole'
+              id={JSON.stringify({ x: index, y: this.props.rowNumber })}
+              style={{ background: color }}
+              onClick={this.props.clickCallback}
+            ></span>
+          );
+        })}
+      </div>
+    );
+  }
+}
 
 interface BoardProps {
   board: Array<[Number, Number]>;
 }
 
-interface BoardState {}
+interface BoardState {
+  rows: Array<Array<Number>>;
+}
 
 class Board extends React.Component<BoardProps, BoardState> {
-  private canvasRef: React.RefObject<HTMLCanvasElement>;
+  rowLengths: Array<Number>;
+  colors: Array<String>;
   constructor(props: BoardProps) {
     super(props);
-    this.canvasRef = createRef();
+    this.rowLengths = [
+      1, 2, 3, 4, 13, 12, 11, 10, 9, 10, 11, 12, 13, 4, 3, 2, 1,
+    ];
+    this.colors = ['hsl(240, 72%, 67%)', 'hsl(240, 80%, 40%)'];
+    let rows: Array<Array<Number>> = Array<Array<Number>>(17);
+    for (let i = 0; i < 17; i++) {
+      rows[i] = Array(this.rowLengths[i]).fill(0);
+    }
+    this.state = {
+      rows: rows,
+    };
   }
 
-  draw = (context: CanvasRenderingContext2D) => {
-    context.beginPath();
-    context.moveTo(100, 100);
-    context.lineTo(100, 300);
-    context.lineTo(300, 300);
-    context.closePath();
-
-    // the outline
-    context.lineWidth = 10;
-    context.strokeStyle = '#666666';
-    context.stroke();
-
-    // the fill color
-    context.fillStyle = '#FFCC00';
-    context.fill();
+  onClick = (e: React.MouseEvent) => {
+    const target = e.nativeEvent.target as HTMLSpanElement;
+    const clicked = JSON.parse(target.id);
+    let newRows = JSON.parse(JSON.stringify(this.state.rows)); // don't modify in place
+    newRows[clicked.y][clicked.x] = newRows[clicked.y][clicked.x] == 1 ? 0 : 1;
+    this.setState({
+      rows: newRows,
+    });
+    return;
   };
 
-  componentDidMount() {
-    const canvas = this.canvasRef.current;
-    const context = canvas!.getContext('2d');
-    if (context) {
-      this.draw(context);
-    }
-  }
-
   render() {
-    return <canvas ref={this.canvasRef} className='Board'></canvas>;
+    console.log(this.state.rows);
+    return (
+      <div className='Board'>
+        {this.state.rows.map((value: Array<Number>, index: Number) => {
+          return (
+            <Row
+              row={value}
+              rowNumber={index}
+              colors={this.colors}
+              clickCallback={this.onClick}
+            ></Row>
+          );
+        })}
+      </div>
+    );
   }
 }
 
